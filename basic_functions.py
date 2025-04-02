faaebf0992cafc32768a13edd50d6cdacb862ac8
Ignacio <ignacioojea@gmail.com>
2025-04-02 15:44:39 +0200

    Update basic_functions.py

--- basic_functions.py
+++ basic_functions.py
@@ -28,8 +28,11 @@ def find_all_subsets_without_empty(input_set):
     # Convert to sets and exclude the empty set
     return [set(subset) for subset in subsets if subset]  # if subset Exclude the empty set
 
-# Example usage
-example_set = {'a', 'b', 'c'}
-all_subsets = find_all_subsets_without_empty(example_set)
-print(all_subsets)
+def generate_random_choice_function(input_set):
+  all_subsets = find_all_subsets_without_empty(input_set)
+  for subset in all_subsets:
+    all_subsubsets = find_all_subsets_without_empty(subset)
+    choice_function[frozenset(subset)] = random.choice(all_subsubsets)
+  return choice_function
+
 
